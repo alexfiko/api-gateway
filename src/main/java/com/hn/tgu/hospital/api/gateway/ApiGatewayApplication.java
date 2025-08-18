8c2b48ec185374593aae35db6aff5373356c8629
Alex Espinal <alex.espinal.hn@gmail.com>
2025-08-17 19:26:41 -0600

    si

--- src/main/java/com/hn/tgu/hospital/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/hn/tgu/hospital/api/gateway/ApiGatewayApplication.java
@@ -2,17 +2,14 @@ package com.hn.tgu.hospital.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient 
 public class ApiGatewayApplication {
-
-	public static void main(String[] args) {
-		// Retrieve execution profile from environment variable. Otherwise, default profile is used.
-		String profile = System.getenv("PROFILE");
-		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        String profile = System.getenv("PROFILE");
+        if (profile != null) {
+            System.setProperty("spring.profiles.active", profile);
+        }
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
