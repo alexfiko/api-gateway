e5a95b44e3f1e1f7cdae1371cfb74bf557608cec
Alex Espinal <alex.espinal.hn@gmail.com>
2025-08-17 19:29:09 -0600

    uu

--- src/main/java/com/hn/tgu/hospital/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/hn/tgu/hospital/api/gateway/ApiGatewayApplication.java
@@ -3,7 +3,11 @@ package com.hn.tgu.hospital.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class
+    }
+)
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         String profile = System.getenv("PROFILE");
